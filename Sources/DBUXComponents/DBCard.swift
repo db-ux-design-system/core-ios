af58d897061b136f669448162c5e515e516d159b
Andreas Herschbach <andreas.herschbach@deutschebahn.com>
2026-03-30 13:14:07 +0200

    added DBCard

---
+++ Sources/DBUXComponents/DBCard.swift
@@ -0,0 +1,165 @@
+//
+//  Copyright 2024 by DB Systel GmbH
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//          http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import SwiftUI
+import DBUXFoundation
+
+enum DBCardElevation: CaseIterable {
+    case level1
+    case level2
+    case level3
+    
+    internal func previewName(def: DBCardElevation = .level1) -> String {
+        var name = "\(def == self ? "(Def) " : "")"
+
+        switch self {
+        case .level1:
+            name.append("1")
+        case .level2:
+            name.append("2")
+        case .level3:
+            name.append("3")
+        }
+        
+        return name
+    }
+}
+
+enum DBCardSpacing: CaseIterable {
+    case small
+    case medium
+    case large
+    case none
+    
+    internal func previewName(def: DBCardSpacing = .small) -> String {
+        var name = "\(def == self ? "(Def) " : "")"
+
+        switch self {
+        case .small:
+            name.append("Small")
+        case .medium:
+            name.append("Medium")
+        case .large:
+            name.append("Large")
+        case .none:
+            name.append("None")
+        }
+        
+        return name
+    }
+    
+    var shortPreviewName: String {
+        switch self {
+        case .small:
+            return "sm"
+        case .medium:
+            return "md"
+        case .large:
+            return "lg"
+        case .none:
+            return "None"
+        }
+    }
+}
+
+struct DBCard<Content: View>: View {
+    @Environment(\.theme) var theme
+    
+    var elevation: DBCardElevation = .level1
+    var spacing: DBCardSpacing = .small
+    @ViewBuilder let content: () -> Content
+    
+    private var padding: CGFloat {
+        switch spacing {
+        case .small:
+            return theme.dimensions.spacing.fixedSm
+        case .medium:
+            return theme.dimensions.spacing.fixedMd
+        case .large:
+            return theme.dimensions.spacing.fixedLg
+        case .none:
+            return 0
+        }
+    }
+    
+    private var backgroundColor: Color {
+        switch elevation {
+        case .level1:
+            return theme.activeColor.basic.background.level1.default
+        case .level2:
+            return theme.activeColor.basic.background.level2.default
+        case .level3:
+            return theme.activeColor.basic.background.level3.default
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: theme.dimensions.border.radiusSm)
+                .fill(backgroundColor)
+                .stroke(theme.activeColor.onBgBasicEmphasis60Default, lineWidth: theme.dimensions.border.height3xs)
+                .padding(0.5)
+            
+            content()
+                .padding(padding)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: theme.dimensions.border.radiusSm))
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    PreviewTemplate(
+        title: "DBCard",
+        previewVariants: DBCardElevation.allCases.map({ elevation in
+            [
+                AnyView(DBCard(elevation: elevation) {})
+            ]
+        }),
+        previewProperties: [
+            PreviewPropertiesSection(
+                name: "Elevation Level",
+                content: DBCardElevation.allCases.map({ elevation in
+                    PreviewPropertiesElement(
+                        description: elevation.previewName(),
+                        content: {
+                            DBCard(elevation: elevation) {}
+                                .frame(width: 100, height: 100)
+                        }
+                    )
+                })
+            ),
+            PreviewPropertiesSection(
+                name: "Spacing",
+                content: DBCardSpacing.allCases.map({ spacing in
+                    PreviewPropertiesElement(
+                        description: spacing.previewName(),
+                        content: {
+                            DBCard(spacing: spacing) {
+                                Color(red: 231/255, green: 0, blue: 235/255)
+                                    .opacity(0.32)
+                                     
+                                Text(spacing.shortPreviewName)
+                                    .foregroundColor(Color(red: 214/255, green: 0, blue: 214/255))
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            }
+                                .frame(width: 100, height: 100)
+                        }
+                    )
+                })
+            ),
+        ],
+    )
+}
